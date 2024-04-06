1b13e832fbde304e0806e0e7049122075bd8b163
arashi2003 <quilabmacmac35@gmail.com>
2024-04-06 23:27:50 +0800

    w

--- users/nurse/appointment.php
+++ users/nurse/appointment.php
@@ -303,25 +303,32 @@ if ($approved_pages > 4) {
                                             </thead>
                                             <tbody>
                                                 <?php
+                                                $sql = "SELECT ap.id, ap.date, ap.patient, ap.med_1, ap.mqty_1, ap.med_2, ap.mqty_2, ap.med_3, ap.mqty_3, ap.med_4, ap.mqty_4, ap.med_5, ap.mqty_5, ap.sup_1, ap.sqty_1, ap.sup_2, ap.sqty_2, ap.sup_3, ap.sqty_3, ap.sup_4, ap.sqty_4, ap.sup_5, ap.sqty_5, p.purpose, t.type, ap.time_from, ap.time_to, ap.physician, ap.status, ac.firstname,  ac.middlename, ac.lastname, ac.campus, ac.email 
+                                                FROM appointment ap 
+                                                INNER JOIN account ac ON ac.accountid = ap.patient 
+                                                INNER JOIN appointment_purpose p ON p.id = ap.purpose 
+                                                INNER JOIN appointment_type t ON t.id = p.type";
+
                                                 if (isset($_GET['patient']) && $_GET['patient'] != '') {
+                                                    // Filter by patient name
                                                     $patient = $_GET['patient'];
-                                                    $count = 1;
-                                                    $sql = "SELECT ap.id, ap.date, ap.patient, ap.med_1, ap.mqty_1, ap.med_2, ap.mqty_2, ap.med_3, ap.mqty_3, ap.med_4, ap.mqty_4, ap.med_5, ap.mqty_5, ap.sup_1, ap.sqty_1, ap.sup_2, ap.sqty_2, ap.sup_3, ap.sqty_3, ap.sup_4, ap.sqty_4, ap.sup_5, ap.sqty_5, p.purpose, t.type, ap.time_from, ap.time_to, ap.physician, ap.status, ac.firstname,  ac.middlename, ac.lastname, ac.campus, ac.email FROM appointment ap INNER JOIN account ac on ac.accountid=ap.patient INNER JOIN appointment_purpose p ON p.id=ap.purpose INNER JOIN appointment_type t ON t.id=p.type WHERE  CONCAT(ac.firstname, ac.middlename,ac.lastname) LIKE '%$patient%' AND ap.status='PENDING' AND ac.campus='$campus' ORDER BY ap.time_from, ap.time_to  LIMIT $start, $rows_per_page";
-                                                    $result = mysqli_query($conn, $sql);
-                                                } elseif (isset($_GET['date']) && $_GET['date'] != '' || isset($_GET['physician']) && $_GET['physician'] != '') {
-                                                    $date = isset($_GET['date']) ? $_GET['date'] : '';
-                                                    $physician = isset($_GET['physician']) ? $_GET['physician'] : '';
-                                                    $count = 1;
-                                                    $sql = "SELECT ap.id, ap.date, ap.patient, ap.med_1, ap.mqty_1, ap.med_2, ap.mqty_2, ap.med_3, ap.mqty_3, ap.med_4, ap.mqty_4, ap.med_5, ap.mqty_5, ap.sup_1, ap.sqty_1, ap.sup_2, ap.sqty_2, ap.sup_3, ap.sqty_3, ap.sup_4, ap.sqty_4, ap.sup_5, ap.sqty_5, p.purpose, t.type, ap.time_from, ap.time_to, ap.physician, ap.status, ac.firstname,  ac.middlename, ac.lastname, ac.campus, ac.email FROM appointment ap INNER JOIN account ac on ac.accountid=ap.patient INNER JOIN appointment_purpose p ON p.id=ap.purpose INNER JOIN appointment_type t ON t.id=p.type WHERE ap.date = '$date' or ap.physician = '$physician' AND ac.campus='$campus' AND ap.status='PENDING' ORDER BY ap.time_from, ap.time_to  LIMIT $start, $rows_per_page";
-                                                    $result = mysqli_query($conn, $sql);
+                                                    $sql .= " WHERE CONCAT(ac.firstname, ac.middlename, ac.lastname) LIKE '%$patient%' AND ap.status = 'PENDING' AND ac.campus = '$campus'";
+                                                } elseif (isset($_GET['date']) && $_GET['date'] != '') {
+                                                    // Filter by date
+                                                    $date = $_GET['date'];
+                                                    $sql .= " WHERE ap.date = '$date' AND ap.status = 'PENDING' AND ac.campus = '$campus'";
+                                                } elseif (isset($_GET['physician']) && $_GET['physician'] != '') {
+                                                    // Filter by physician
+                                                    $physician = $_GET['physician'];
+                                                    $sql .= " WHERE ap.physician = '$physician' AND ap.status = 'PENDING' AND ac.campus = '$campus'";
                                                 } else {
-                                                    $count = 1;
-                                                    $sql = "SELECT ap.id, ap.date, ap.patient, ap.chiefcomplaint, ap.med_1, ap.mqty_1, ap.med_2, ap.mqty_2, ap.med_3, ap.mqty_3, ap.med_4, ap.mqty_4, ap.med_5, ap.mqty_5, ap.sup_1, ap.sqty_1, ap.sup_2, ap.sqty_2, ap.sup_3, ap.sqty_3, ap.sup_4, ap.sqty_4, ap.sup_5, ap.sqty_5, p.purpose, t.type, ap.time_from, ap.time_to, ap.physician, ap.status, ac.firstname,  ac.middlename, ac.lastname, ac.campus, ac.email FROM appointment ap INNER JOIN account ac on ac.accountid=ap.patient INNER JOIN appointment_purpose p ON p.id=ap.purpose INNER JOIN appointment_type t ON t.id=p.type 
-                                                    
-                                                    
-                                                     WHERE ap.status='PENDING' AND ac.campus='$campus' ORDER BY ap.date, ap.time_from, ap.time_to  LIMIT $start, $rows_per_page";
-                                                    $result = mysqli_query($conn, $sql);
+                                                    // No filter, retrieve all pending appointments
+                                                    $sql .= " WHERE ap.status = 'PENDING' AND ac.campus = '$campus'";
                                                 }
+
+                                                $sql .= " ORDER BY ap.date, ap.time_from, ap.time_to LIMIT $start, $rows_per_page";
+
+                                                $result = mysqli_query($conn, $sql);
                                                 if ($result) {
                                                     if (mysqli_num_rows($result) > 0) {
                                                         foreach ($result as $data) {
