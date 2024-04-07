5aa852d78e022e7c1371c328d4924d5e759739c8
arashi2003 <quilabmacmac35@gmail.com>
2024-04-07 18:07:03 +0800

    alert

--- users/superadmin/drop.php
+++ users/superadmin/drop.php
@@ -19,15 +19,20 @@ $conn->query("SET FOREIGN_KEY_CHECKS=0");
 // Get a list of tables in the database
 $tables_result = $conn->query("SHOW TABLES");
 if ($tables_result) {
+    $tables_dropped = false; // Flag to indicate if any tables were dropped
     while ($table_row = $tables_result->fetch_row()) {
         $table_name = $table_row[0];
         // Drop each table
         $drop_table_sql = "DROP TABLE $table_name";
         if ($conn->query($drop_table_sql) === TRUE) {
-            $_SESSION['alert'] .= "Table $table_name has been dropped<br>";
+            $tables_dropped = true; // Set the flag to true if any table is dropped
         } else {
             $_SESSION['alert'] .= "Error dropping table $table_name: " . $conn->error . "<br>";
         }
+    }
+
+    if ($tables_dropped) {
+        $_SESSION['alert'] = "Tables has been dropped<br>"; // Display message only if tables were dropped
     }
 } else {
     $_SESSION['alert'] = 'Error getting list of tables: ' . $conn->error;
